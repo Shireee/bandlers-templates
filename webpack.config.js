90cd0de106fd0deeba97ada4bac26d5d1b05cead
Shireee <kirog11az@gmail.com>
2024-02-02 22:22:45 +0300

    Add devserv and copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WebpackDevServer = require('webpack-dev-server');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === 'production'
 
     return{
-        entry: './src/scripts/index.js',
+        entry: './src/index.js',
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: 'bundle.js'
@@ -13,6 +15,17 @@ module.exports = (env, argv) => {
         module: {
             rules: [{test: /\.scss$/, use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'sass-loader']}]
         },
-        plugins: [new MiniCssExtractPlugin({filename: "styles/style.scss"})],
+          plugins: [new MiniCssExtractPlugin({filename: "styles/style.scss"}),
+                    new CopyPlugin({ patterns: [{ from: "./src/index.html", to: "./" }, { from: "./src/img/**", to: "./img", noErrorOnMissing: true }] })
+          ],
+        devServer: {
+            static: {
+              directory: path.resolve(__dirname, './dist'),
+            },
+            historyApiFallback: true, 
+            open: true, 
+            hot: true,
+            port: 8080,
+          },
     }
 }
