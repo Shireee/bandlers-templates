f3812e5afc590af877ae621138516b494048e808
Vladimir Bagdeev <kirog11az@gmail.com>
2024-04-05 15:23:47 +0300

    Fix assets issue, change reset styles, rename dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,41 @@ module.exports = (env, argv) => {
     return{
         entry: './src/index.js',
         output: {
-            path: path.resolve(__dirname, 'dist'),
+            path: path.resolve(__dirname, 'docs'),
             filename: 'bundle.js'
         },
         module: {
-            rules: [{test: /\.scss$/, use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'sass-loader']}]
-        },
-          plugins: [new MiniCssExtractPlugin({filename: "styles/style.scss"}),
-                    new CopyPlugin({ patterns: [{ from: "./src/index.html", to: "./" }, { from: 'img/**', to: './', context: 'src/', noErrorOnMissing: true }]})
+          rules: [
+              {
+                  test: /\.scss$/,
+                  use: [
+                      isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                      'css-loader',
+                      'sass-loader'
+                  ]
+              },
+              {
+                  test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                  type: 'asset/resource',
+                  generator: {
+                      filename: 'fonts/[name][ext]'
+                  }
+              },
+              {
+                test: /\.(jpeg|svg|jpg|webp|png)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[name][ext]'
+                }
+            }
+          ]
+      },
+          plugins: [new MiniCssExtractPlugin({filename: "style/style.css"}),
+                    new CopyPlugin({ patterns: [{ from: "*.html", to: "./", context:'src/'}, { from: 'img/**', to: './', context: 'src/', noErrorOnMissing: true }]})
           ],
         devServer: {
             static: {
-              directory: path.resolve(__dirname, './dist'),
+              directory: path.resolve(__dirname, './docs'),
             },
             historyApiFallback: true, 
             open: true, 
